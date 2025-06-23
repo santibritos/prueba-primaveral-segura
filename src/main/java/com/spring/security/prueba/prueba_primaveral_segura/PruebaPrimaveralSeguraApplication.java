d42ad854ea74113cbb7f37a163ecc74dee2bd78b
santibritos <116766975+santibritos@users.noreply.github.com>
2025-06-23 19:48:46 -0300

    Initial commit

---
+++ src/main/java/com/spring/security/prueba/prueba_primaveral_segura/PruebaPrimaveralSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.prueba.prueba_primaveral_segura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPrimaveralSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaPrimaveralSeguraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/security/prueba/prueba_primaveral_segura/PruebaPrimaveralSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.prueba.prueba_primaveral_segura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaPrimaveralSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
